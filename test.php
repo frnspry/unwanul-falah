5ffa84f7c69d0fbab6abab3086b4529710bc9b6b
frnspry <frnspry@gmail.com>
2025-01-09 20:12:26 +0700

    update test and css

--- test.php
+++ test.php
@@ -2,13 +2,14 @@
 include ("template/header.php");
 ?>
         <main>
-            <div id="carouselExampleSlidesOnly" class="carousel slide carousel-fade" data-bs-ride="carousel" data-bs-pause="false">
+            <div id="carouselMadrasah" class="carousel slide carousel-fade" data-bs-ride="carousel" data-bs-pause="false">
                 <div class="carousel-inner">
                     <div class="carousel-caption text-center">
                         <h3>Welcome to Madrasah</h3>
                         <p>Madrasah Terpadu dan Maju.</p>
-                        <a href="#" class="btn btn-primary">Pendaftaran</a>
+                        <a href="#" class="btn btn-primary border-0">Pendaftaran</a>
                     </div>
+                    <div class="overlay"></div>
                     <div class="carousel-item active">
                     <img src="assets/img/elgato1.png" class="d-block w-100" alt="elg">
                     </div>
@@ -23,6 +24,8 @@ include ("template/header.php");
                     </div>
                 </div>
             </div>
+
+
         </main>
 <?php
 include ("template/footer.php");
