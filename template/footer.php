4fe95b61040970827e200b1fa7d9d610180f2972
Fakutra <152264409+Fakutra@users.noreply.github.com>
2025-01-10 14:43:19 +0700

    naambahin footer

--- template/footer.php
+++ template/footer.php
@@ -1,13 +1,41 @@
 <footer>
-            <div class="container-fluid py-5 bg-img">
-                <div class="content">
-                    <h3>Mari Membangun Negeri</h3>
-                    <h3>Dengan Bergabung Bersama Kami</h3>
-                    <p>Daftar sekarang untuk menjadi siswa hebat</p>
-                    <a href="#" class="btn btn-primary border-0">Pendaftaran</a>
+    <div class="container-fluid py-5 bg-img text-center text-white">
+        <div class="content">
+            <h3>Mari Membangun Negeri</h3>
+            <h3>Dengan Bergabung Bersama Kami</h3>
+            <p>Daftar sekarang untuk menjadi siswa hebat</p>
+            <a href="#" class="btn btn-primary border-0">Pendaftaran</a>
+        </div>
+    </div>
+    <div class="container-fluid py-5 bg-light">
+        <div class="row align-items-center">
+            <!-- Bagian Kontak -->
+            <div class="col-md-6">
+                <div class="contact-info text-left">
+                    <img src="assets/img/logo.png" alt="Logo" class="img-fluid mb-3" style="max-width: 150px;">
+                    <h4>CONTACT</h4>
+                    <p><strong>Pondok Pesantren Daaruttaqwa</strong></p>
+                    <p>Jalan raya Jakarta-Bogor KM 44 Pakansari, Cibinong Bogor, Jawa Barat 16915</p>
+                    <p><i class="bi bi-telephone"></i> 0813-1914-5285</p>
+                    <p><i class="bi bi-envelope"></i> info@daaruttaqwa.sch.id</p>
                 </div>
             </div>
+            <!-- Bagian Peta -->
+            <div class="col-md-6">
+                <div class="map-responsive">
+                    <iframe 
+                        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15847.334735486244!2d106.8462486!3d-6.4814539!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69c1d7e2e31853%3A0xc1cda9829f6eb6aa!2sPondok%20Pesantren%20Daaruttaqwa!5e0!3m2!1sid!2sid!4v1694186242655!5m2!1sid!2sid" 
+                        width="600" 
+                        height="300" 
+                        style="border:0;" 
+                        allowfullscreen="" 
+                        loading="lazy"></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
 </footer>
+
         <!-- Bootstrap JavaScript Libraries -->
         <script
             src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
