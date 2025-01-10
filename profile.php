fcfa849fc7f9b264b7b8e2788c090bc623da4a8d
Fakutra <152264409+Fakutra@users.noreply.github.com>
2025-01-10 14:01:20 +0700

    Menambakan profile.php dan style.css

--- profile.php
+++ profile.php
@@ -25,49 +25,74 @@ include("template/header.php");
     </div>
 
     <!-- History Section -->
-    <div class="section">
-        <div class="content">
-            <h2>SEJARAH</h2>
-            <p>
-                el sejarah 
-            </p>
-            
-        </div>
-    </div>
-
-    <!-- Quote Section -->
-    <div class="section">
-        <div class="quote">
-            <div class="quote-text">
-                <p>
-                    KATA KATANYA MAS
-                </p>
-                <div class="author">
-                    Okta
-                </div>
-                <div class="position">
-                    Sang Project Manager
+    <div class="container py-5" data-aos="fade-up">
+        <div class="section">
+            <div class="content">
+                <img src="assets/img/obama.png" alt="Foto Kepala Sekolah" class="img-fluid">
+                <div>
+                    <h2>SEJARAH</h2>
+                    <p>
+                        el sejarah dasdadasdasda 
+                    </p>
+                    <p>
+                        dadawdasdadasdw
+                    </p>
                 </div>
             </div>
-            <img alt="Photo of a man speaking into a microphone" height="400" src="https://storage.googleapis.com/a1aa/image/f2UyM8ZyPtWPM6PbkaOrFMCi7GXLYCVqEkfoym65yRLzgQDUA.jpg" width="300"/>
         </div>
     </div>
-    <div class="section">
-        <div class="visi">
-            <h2>Visi</h2>
-            <p>
-                el sejarah 
-            </p>
-            
+    <!-- Quote Section -->
+    <div class="container py-5" data-aos="fade-left">
+        <div class="section">
+            <div class="quote">
+                <div class="quote-text">
+                    <p>
+                        KATA KATANYA MAS
+                    </p>
+                    <div class="author">
+                        Okta
+                    </div>
+                    <div class="position">
+                        Sang Project Manager
+                    </div>
+                </div>
+                <img alt="Photo of a man speaking into a microphone" height="400" src="https://storage.googleapis.com/a1aa/image/f2UyM8ZyPtWPM6PbkaOrFMCi7GXLYCVqEkfoym65yRLzgQDUA.jpg" width="300"/>
+            </div>
         </div>
     </div>
-    <div class="section">
-        <div class="misi">
-            <h2>Misi</h2>
-            <p>
-                el sejarah 
-            </p>
-            
+    <div class="container py-5" data-aos="fade-up">
+        <div class="section">
+            <div class="image-row d-flex justify-content-between align-items-center">
+                <div class="col-md-4 text-left text-center">
+                    <img src="assets/img/obama.png" alt="Foto Kepala Sekolah" class="img-fluid">
+                    <p class="mt-3"><strong>Pak Obama</strong><br>Mantan Presiden AS</p>
+                </div>
+                <div class="col-md-4 text-right text-center">
+                    <img src="assets/img/obama.png" alt="Foto Kepala Sekolah" class="img-fluid">
+                    <p class="mt-3"><strong>Pak Obama</strong><br>Mantan Presiden AS</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container py-5" data-aos="fade-right">
+        <div class="section">
+            <div class="visi">
+                <h2>Visi</h2>
+                <p>
+                    el sejarah 
+                </p>
+                
+            </div>
+        </div>
+    </div>
+    <div class="container py-5" data-aos="fade-left">
+        <div class="section">
+            <div class="misi">
+                <h2>Misi</h2>
+                <p>
+                    el sejarah 
+                </p>
+            </div>
         </div>
     </div>
 </main>
