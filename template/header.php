032aa04ec8654bbc998cbc00249c785f4a521196
frnspry <frnspry@gmail.com>
2025-01-12 13:31:14 +0700

    pindahin ke pages

--- template/header.php
+++ template/header.php
@@ -17,7 +17,7 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
         <!-- CSS file -->
-        <link href="assets/css/style.css" rel="stylesheet">
+        <link href="../assets/css/style.css" rel="stylesheet">
 
         <!-- AOS -->
         <link href="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.css" rel="stylesheet">
